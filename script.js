f9501263ac209794ceb82b748737a4a48683881d
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-11-18 13:21:58 -0500

    17

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Funciones compartidas
 function mostrarParte(imageUrl, description) {
     // Set the modal content
     document.getElementById("modalImage").src = imageUrl;
@@ -15,105 +16,29 @@ function closeModal() {
     document.getElementById("modal").style.display = "none";
 }
 
+function solicitarCotizacion(partName) {
+    window.location.href = "https://api.whatsapp.com/send?phone=123456789&text=Hola,%20estoy%20interesado%20en%20la%20parte:%20" + partName;
+}
+
+// Función para mostrar solo descripción corta en las tarjetas
+function mostrarDescripcionCorta() {
+    document.querySelectorAll('.card').forEach(function (card) {
+        var description = card.querySelector('p').innerText;
+        var shortenedDescription = description.length > 50 ? description.substring(0, 50) + '...' : description;
+        card.querySelector('p').innerText = shortenedDescription;
+    });
+}
+
+// Llamada a la función al cargar la página
+mostrarDescripcionCorta();
+
+// Event listeners
 document.querySelectorAll('.card').forEach(function (card) {
     card.addEventListener('click', function (event) {
-        if (event.target.tagName.toLowerCase() === 'img') {
-            var imageUrl = card.querySelector('img').src;
-            var description = card.querySelector('p').innerText;
-            mostrarParte(imageUrl, description);
-        }
+        var description = card.querySelector('p').innerText;
+        mostrarParte(card.querySelector('img').src, description);
     });
 });
-
-function solicitarCotizacion() {
-    window.location.href = "https://api.whatsapp.com/send?phone=123456789&text=Hola";
-}
-
-var preciosEjemplo = {
-    'puertas': 100,
-    'capo': 150,
-    'atras': 120,
-    'techo': 200
-};
-
-function mostrarPrecios(partName, description) {
-    var pricingBody = document.getElementById("pricingBody");
-    pricingBody.innerHTML = '';
-
-    if (preciosEjemplo.hasOwnProperty(partName.toLowerCase())) {
-        var row = document.createElement("tr");
-        var partCell = document.createElement("td");
-        partCell.innerText = partName;
-        var descriptionCell = document.createElement("td");
-        descriptionCell.innerText = description;
-        var priceCell = document.createElement("td");
-        var precioBase = preciosEjemplo[partName.toLowerCase()];
-        var precioCalculado = calcularPrecio(precioBase, description);
-        priceCell.innerText = '$' + precioCalculado.toFixed(2);
-        row.appendChild(partCell);
-        row.appendChild(descriptionCell);
-        row.appendChild(priceCell);
-        pricingBody.appendChild(row);
-    } else {
-        var noPriceRow = document.createElement("tr");
-        var noPriceCell = document.createElement("td");
-        noPriceCell.colSpan = 3;
-        noPriceCell.innerText = "Pricing information not available.";
-        noPriceRow.appendChild(noPriceCell);
-        pricingBody.appendChild(noPriceRow);
-    }
-}
-
-function buscarPartes(termino) {
-    var tarjetas = document.querySelectorAll('.card');
-    var resultadosBusqueda = document.getElementById('resultadosBusqueda');
-    resultadosBusqueda.innerHTML = '';
-
-    var resultadosEncontrados = false;
-
-    tarjetas.forEach(function (tarjeta) {
-        var idParte = tarjeta.id;
-        var textoParte = tarjeta.querySelector('h3').innerText.toLowerCase();
-
-        if (idParte.includes(termino.toLowerCase()) || textoParte.includes(termino.toLowerCase())) {
-            resultadosEncontrados = true;
-
-            var imageUrl = tarjeta.querySelector('img').src;
-            var description = tarjeta.querySelector('p').innerText;
-
-            var resultado = document.createElement('div');
-            resultado.innerHTML = `<img src="${imageUrl}" alt="${idParte}"><p>${description}</p>`;
-            resultadosBusqueda.appendChild(resultado);
-
-            tarjeta.style.display = 'block';
-        } else {
-            tarjeta.style.display = 'none';
-        }
-    });
-
-    // Ocultar la sección de resultados si no se encontraron resultados
-    if (!resultadosEncontrados) {
-        resultadosBusqueda.innerHTML = 'No se encontraron resultados.';
-    }
-}
-
-function dejarComentario() {
-    var nombre = document.getElementById('nombre').value;
-    var comentario = document.getElementById('comentario').value;
-    var danio = document.getElementById('danio').value;
-    var precio = calcularPrecio(danio);
-    var resultadosTable = document.querySelector('#resultados table');
-    var newRow = resultadosTable.insertRow(-1);
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-
-    cell1.textContent = danio;
-    cell2.textContent = '$' + precio.toFixed(2);
-
-    document.getElementById('nombre').value = '';
-    document.getElementById('comentario').value = '';
-    document.getElementById('danio').value = '';
-}
 
 document.querySelector('.menu-icon').addEventListener('click', function () {
     document.querySelector('.menu-items').classList.toggle('show');
@@ -128,4 +53,56 @@ document.getElementById('busqueda').addEventListener('input', function () {
         buscarPartes(termino);
     }
 });
+function buscarPartes(termino) {
+    // Itera sobre cada tarjeta
+    document.querySelectorAll('.card').forEach(function (card) {
+        var description = card.querySelector('p').innerText.toLowerCase();
 
+        // Verifica si la descripción de la tarjeta incluye el término de búsqueda
+        if (description.includes(termino.toLowerCase())) {
+            card.style.display = 'block'; // Muestra la tarjeta si hay coincidencia
+        } else {
+            card.style.display = 'none'; // Oculta la tarjeta si no hay coincidencia
+        }
+    });
+}
+// Cierra la ventana modal al tocar la pantalla
+document.getElementById("modal").addEventListener('click', function (event) {
+    if (event.target === this) {
+        closeModal();
+    }
+});
+// Event listeners
+document.querySelectorAll('.card').forEach(function (card) {
+    card.addEventListener('click', function (event) {
+        var description = card.querySelector('p').innerText;
+        mostrarParte(card.querySelector('img').src, description);
+    });
+});
+// Funciones compartidas
+function mostrarParte(imageUrl, description) {
+    // Set the modal content
+    document.getElementById("modalImage").src = imageUrl;
+    document.getElementById("modalDescription").innerText = description;
+
+    // Show the modal
+    document.getElementById("modal").style.display = "flex";
+
+    // Populate the pricing table
+    mostrarPrecios(description);
+}
+
+// Event listeners
+document.querySelectorAll('.card').forEach(function (card) {
+    card.addEventListener('click', function (event) {
+        var description = card.querySelector('p').innerText;
+        
+        // Utiliza el nombre de la tarjeta como parte del nombre de la imagen
+        var imageName = card.id + ".png";
+        var imageUrl = "images/" + imageName;
+
+        mostrarParte(imageUrl, description);
+    });
+});
+
+// Otras funciones y eventos no modificados
